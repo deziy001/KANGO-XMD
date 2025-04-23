02b10642c67ab3c41fd468d984284497d0b1fa33
deziy001 <issabellakate001@gmail.com>
2025-04-23 17:32:22 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,7 +14,7 @@ const { color } = require('./lib/color')
 if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 
-//--------------------[ SESSION ID ]----------------------//
+//--------------------[ SESSKANGO~ct5SDS4B#aqpVJHKcbTjHiZzhdmy31fVddedfMUhjI27LuBDpmE0ION ID ]----------------------//
 
 global.SESSION_ID = process.env.SESSION_ID || '' 
 //Enter your 𝙆𝘼𝙉𝙂𝙊-𝙓𝙈𝘿 session id here; must start with KANGO~
@@ -25,11 +25,11 @@ global.botname = process.env.BOT_NAME || '𝙆𝘼𝙉𝙂𝙊-𝙓𝙈𝘿'
 
 //-----------------[ OWNER NUMBER ]------------------//
 
-global.ownernumber = process.env.OWNER_NUMBER || '233509977126' 
+global.ownernumber = process.env.OWNER_NUMBER || '2347053267036' 
 
 //-----------------[ OWNER NAME ]------------------//
 
-global.ownername = process.env.OWNER_NAME || 'Hector Manuel' 
+global.ownername = process.env.OWNER_NAME || 'Deziy' 
 
 //------------[ STICKER PACKNAME ]-----------------//
 
